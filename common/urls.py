2aea4534df86edf54153b2a043c7916a6236ee25
AntonBulakhov <frostanton09@gmail.com>
2021-03-13 14:38:14 +0300

    Verstka

--- common/urls.py
+++ common/urls.py
@@ -1,11 +1,14 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from FreskiProject import settings
 from common import views
 
 app_name = 'common_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('contacts', views.contacts, name='contacts'),
-    path('examples', views.examples, name='examples'),
-]
+                  path('', views.index, name='index'),
+                  path('about', views.about, name='about'),
+                  path('contacts', views.contacts, name='contacts'),
+                  path('examples', views.examples, name='examples'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
