94e8c79ea7c07892b98c90654e81dcd859c516e8
Anton <44256773+iWebshark@users.noreply.github.com>
2023-12-02 21:41:47 +0300

    Update settings.py

--- FreskiProject/settings.py
+++ FreskiProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's2($ww^nqnbv(o*_7sc4-3zq=*6y#jb0ob(x+2gyug&=78&aty'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
